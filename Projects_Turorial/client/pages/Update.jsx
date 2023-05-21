24198c29e2cea8760f010b8234ddb8eda3f52d3b
Marouf <maroufibrahimi7264@gmail.com>
2023-05-21 11:04:56 -0700

    created Update component

--- Projects_Turorial/client/pages/Update.jsx
+++ Projects_Turorial/client/pages/Update.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from "react"
 
 const Update = () => {
   return (
-    <div>Update</div>
+    <div className="Update">
+      <h1>Update New Book</h1>
+      <input type="text" name="title" placeholder="title" />
+      <input type="text" name="desc" placeholder="desc" />
+      <input type="number" name="price" placeholder="price" />
+      <input type="text" name="cover" placeholder="cover" />
+      <button>Update</button>
+    </div>
   )
 }
 
