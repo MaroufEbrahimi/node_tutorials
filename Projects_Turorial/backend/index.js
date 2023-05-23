0a436b1059af85785b70d1ac9bd38e82c01ebc1a
Marouf <maroufibrahimi7264@gmail.com>
2023-05-22 21:51:49 -0700

    connection to database

--- Projects_Turorial/backend/index.js
+++ Projects_Turorial/backend/index.js
@@ -1,7 +1,15 @@
-import express from 'express'
+import express from "express"
+import mysql from "mysql"
 
 const app = express()
 
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test",
+})
+
 app.listen(8080, () => {
-   console.log('connected to backend');
+  console.log("connected to backend")
 })
