cd1fa96f2e2fd98e002d4d4ca58f1975d5faa150
Marouf <maroufibrahimi7264@gmail.com>
2023-05-22 22:12:49 -0700

    get two route

--- Projects_Turorial/backend/index.js
+++ Projects_Turorial/backend/index.js
@@ -10,6 +10,18 @@ const db = mysql.createConnection({
   database: "test",
 })
 
+app.get("/", (req, res) => {
+  res.json("Hello this is the backend")
+})
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books"
+  db.query(q, (err, data) => {
+    if (err) return res.json(err)
+    return res.json(data)
+  })
+})
+
 app.listen(8080, () => {
   console.log("connected to backend")
 })
