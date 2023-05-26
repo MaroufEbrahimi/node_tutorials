8d90747cca7dc5f48fdc7bdd56affe88ac449482
Marouf <maroufibrahimi7264@gmail.com>
2023-05-25 23:59:36 -0700

    some change

--- Projects_Turorial/client/pages/Books.jsx
+++ Projects_Turorial/client/pages/Books.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from "react"
 
 const Books = () => {
-  return (
-    <div>Books</div>
-  )
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }, [])
+  return <div>Books</div>
 }
 
 export default Books
