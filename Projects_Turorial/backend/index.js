19573369913fb21b78364a82f97f4b16a00383a6
Marouf <maroufibrahimi7264@gmail.com>
2023-05-25 23:59:25 -0700

    added post query

--- Projects_Turorial/backend/index.js
+++ Projects_Turorial/backend/index.js
@@ -6,12 +6,13 @@ const app = express()
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "test",
+  password: "fam##00%%",
 })
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
-  res.json("Hello this is the backend")
+  res.json("hi, this is home page")
 })
 
 app.get("/books", (req, res) => {
@@ -22,6 +23,20 @@ app.get("/books", (req, res) => {
   })
 })
 
-app.listen(8080, () => {
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)"
+  const values = [
+    "title form backend",
+    "desc form backend",
+    "cover picture form backend",
+  ]
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err)
+    return res.json(data)
+  })
+})
+
+app.listen(8800, () => {
   console.log("connected to backend")
 })
